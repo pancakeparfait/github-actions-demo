19f91dc0119472fef65ecf89b3834db66e0a422a
Jared Wooten <pancakeparfait@gmail.com>
2022-01-29 00:26:46 -0500

    Updated header in App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+test('renders hello world header', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/hello world/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
